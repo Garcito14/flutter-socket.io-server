c8783594c0fa2a5518f614eb829153ec3961cc2d
unknown <felipe.garces@socialway.cl>
2020-10-14 18:47:18 -0300

    hola

---
+++ sockets/socket.js
@@ -0,0 +1,13 @@
+// Mensajes de Sockets 
+
+const {io} = require('../index');
+io.on('connection', client => {
+    console.log('Cliente conectado');
+
+client.on('disconnect', () => console.log('cliente desconectado'));
+
+client.on('mensaje',(payload) =>{
+    console.log('mensaje',payload);
+    io.emit('mensaje',{admin:'nuevo mensaje'});
+});
+});
